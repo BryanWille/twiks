166d0f7f82d77f7734353cef2849c6cc6d5d7077
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-11-20 04:45:19 -0300

    Começando o projetinho benkars

---
+++ main.py
@@ -0,0 +1,17 @@
+import tweepy
+import keys
+
+def api():
+    auth = tweepy.OAuth1UserHandler(keys.api_key, keys.api_secret)
+    auth.set_access_token(keys.access_token, keys.access_token_secret)
+    
+    return tweepy.API(auth)
+
+def tweet(api: tweepy.API, message: str):
+    api.update_status(message)
+    print("tweetei com sucesso!")
+    
+
+if __name__ == '__main__':
+    api = api()
+    tweet(api, "Isso foi twiteado do python")
