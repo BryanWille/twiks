b8419fe29a96a6a5bd2fcdff632a73501ca41ae7
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-11-21 23:31:35 -0300

    bot já está retuitando

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
 import tweepy
 import keys
-from tweepy import user;
+
 
 def api():
     auth = tweepy.OAuth1UserHandler(keys.api_key, keys.api_secret)
@@ -10,12 +11,16 @@ def api():
 
 def tweet(api: tweepy.API, message: str):
     api.update_status(message)
+    api.get_favorites
     print("tweetei com sucesso!")
     
 
 if __name__ == '__main__':
     api = api()
-    #tweet(api, "Isso foi twiteado do python")
-    resposta = tweepy.Client.get_liked_tweets(
-    "Tweepy", id=["bernardokbt"])
-    print(resposta)
+    likes_bernardo = api.get_favorites(id = 2882428500, count=30)
+    for likes in likes_bernardo:
+        try:
+             api.unretweet(likes._json["id"])
+             print("RT concluido")
+        except:
+            ""
