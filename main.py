c3db681b0c2d38012e2bafce8d5b6678a50af3b2
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-11-25 00:25:12 -0300

    likes saved
    All likes are saved, but i have to do something to fetch that data

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import services.database as db
-import models.user as twitteruser
 import json
 import tweepy
 import keys.keys as keys
@@ -26,14 +25,26 @@ def unretweet(api: tweepy.API, tweetID: int):
 def getUserData(api: tweepy.API, username: str):
     return api.get_user(screen_name = username)
 
-def writeDataJson(data, filename = str):
-    file = open(filename+".json", "w")
-    file.write(json.dumps(data))
+def getAllLikes(api: tweepy.API, user_id, like_count: int):
+    return api.get_favorites(id=user_id, count=like_count)
 
-
-if __name__ == '__main__':
+def createUserDataBase(user_name):
     api = api()
-    usuario = getUserData(api, "bernardokbt")
-    pessoa = twitteruser.User.mapToUser(user_map=usuario._json)
-    mapa_pessoa = pessoa.userToMap()
-    db.Database.createUserInfo(mapa_pessoa)
+    db.Database.createUserInfo(getUserData(api, user_name)._json)
+
+def createFavDataBase(user_name):
+    api = api()
+    user = getUserData(api, user_name)
+    favourite_count = getAllLikes(api, user['screen_name'], user['favourites_count'])
+    for c in range(0, len(favourite_count)):
+        db.Database.createStatusInfo(favourite_count[c]._json, user_name)
+    
+if __name__ == '__main__':
+    user_name = "sr_wille"
+    # createUserDataBase(user_name=user_name)
+    
+    # createFavDataBase(user_name)
+
+    fav_list = db.Database.retrieveStatusInfo(username=user_name)
+    print(fav_list)
+    
--- services/database.py
+++ services/database.py
@@ -3,19 +3,25 @@ from firebase_admin import firestore
 from firebase_admin import credentials
 
 class Database:
+    cred = credentials.Certificate("keys/firebase-key.json")
+    app = firebase_admin.initialize_app(cred)
 
     def createUserInfo(user_map):
-        cred = credentials.Certificate("keys/firebase-key.json")
-        app = firebase_admin.initialize_app(cred)
-        db = firestore.client()
-        doc_ref = db.collection('users').document(user_map['screen_name'])
+        doc_ref = firestore.client().collection('users').document(user_map['screen_name'].lower()).collection("user_info").document(user_map['id_str'])
         doc_ref.set(user_map)
-        print("User Sent Sucessfully")
-        
-    def createLikeInfo(user_map):
-        cred = credentials.Certificate("keys/firebase-key.json")
-        app = firebase_admin.initialize_app(cred)
-        db = firestore.client()
-        doc_ref = db.collection('users').document(user_map['screen_name'])
-        doc_ref.set(user_map)
-        print("User Sent Sucessfully")
+        print("User Sucessfully Sent")
+
+    def createStatusInfo(user_map, username: str):
+        try:
+            doc_ref = firestore.client().collection('users').document(username.lower()).collection("favourites").document(user_map['user']['screen_name']).collection('posts').document(user_map['id_str'])
+            doc_ref.set(user_map)
+            print("Favourite Sucessfully Sent")
+        except Exception as e:
+            print("Favourite not Sent")
+            print(e)
+            print(f"Tweet id: {user_map['id']}")
+    
+    def retrieveStatusInfo(username: str):
+        docs = firestore.client().collection('users').document(username.lower()).collection('favourites').stream()
+        for doc in docs:
+            print()
