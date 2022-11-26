85e60cd846e6360352ae70b89271c1e9a326f971
Bryan Wille <84272231+BryanWille@users.noreply.github.com>
2022-11-26 18:54:53 -0300

    mundado algumas coisas

--- README.md
+++
@@ -1,2 +0,0 @@
-# benkars-bot
-O Benkars é um bot que reposta tudo que o bernardo curte no twitter
--- services/database.py
+++ services/database.py
@@ -3,30 +3,39 @@ from firebase_admin import firestore
 from firebase_admin import credentials
 
 class Database:
-    cred = credentials.Certificate("keys/firebase-key.json")
-    app = firebase_admin.initialize_app(cred)
+    
+    def __init__(self, user_name):
+        self.username = user_name
+        self.cred = credentials.Certificate("keys/firebase-key.json")
+        self.app = firebase_admin.initialize_app(self.cred)
+        self.db = firestore.client()
 
     def createUserInfo(user_map):
         doc_ref = firestore.client().collection('users').document(user_map['screen_name'].lower()).collection("user_info").document(user_map['id_str'])
         doc_ref.set(user_map)
         print("User Sucessfully Sent")
 
-    def createStatusInfo(user_map, username: str):
+    def createStatusInfo(self, user_map):
         try:
-            doc_ref = firestore.client().collection('users').document(username.lower()).collection("favourites").document(user_map['id_str'])
+            doc_ref = firestore.client().collection('users').document(self.username.lower()).collection("favourites").document(user_map['id_str'])
             doc_ref.set(user_map)
             print("Favourite Sucessfully Sent")
         except Exception as e:
             print("Favourite not Sent")
             print(e)
             print(f"Tweet id: {user_map['id']}")
+            
+    def createRetweetInfo(self, user_map: map):
+        print()
     
-    def retrieveStatusInfo(username: str):
+    
+    def retrieveStatusInfo(self):
         db = firestore.client()            
-        docs = db.collection(u'users').document(username).collection('favourites').stream()
+        docs = db.collection(u'users').document(self.username).collection('favourites').stream()
         
         fav_list = []
         for doc in docs:
             fav_list.append(doc.to_dict())
             
         return fav_list
+
